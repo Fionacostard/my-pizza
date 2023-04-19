e35fa286e289463d64d2240bc0c36a3eab1209b1
Fiona Costard <costardf@yahoo.fr>
2023-04-19 18:49:09 +0200

    corrected weird bug

--- sources/fridge.py
+++ sources/fridge.py
@@ -18,9 +18,14 @@ class NotEnoughException(Exception):
 
 
 class Fridge:
-    def __init__(self, default: int = 0):
-        self._ingredients: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, default)
-        self._count: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, 0)
+    def __init__(self, default_ingredients: int = 0, default_sauce: int = 4):
+        if default_ingredients < 0:
+            default_ingredients = 0
+        if default_sauce < 0:
+            default_sauce = 0
+        self._ingredients: Dict[str,int] = dict.fromkeys(ALL_INGREDIENTS, default_ingredients)
+        self._sauces: Dict[str, int] = dict.fromkeys(ALL_SAUCES, default_sauce)
+        self._count: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS,0)
 
     @property
     def is_empty(self) -> bool:
